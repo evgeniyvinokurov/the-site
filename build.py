bfc291fb519717c9f1fb45185d3bd73050c5b7f0
evgenii <evgenii@localhost.localdomain>
2025-04-23 11:40:09 +0300

    laravel sample

--- build.py
+++ build.py
@@ -114,6 +114,7 @@ projects = [
         "index.html"
     ], "github": True},
     {"name": "users","tags": ["js", "angular", "markup"], "dirs": ["app"], "files": [], "baseUrl": "app/", "github": True},
+    {"name": "laravel-sample", "tags": ["php", "sqlite", "api", "sql", "laravel"], "files": [], "github": True},
     {"name": "deezer-api-albums-php", "tags": ["php", "sqlite", "api", "sql"], "files": [], "url": "http://www.evgeniyvinokurov.byethost9.com/albums/", "github": True},
     {"name": "xmla", "tags": ["python", "js", "xml", "bottle", "catalog", "markup", "tests", "docker", "import"], "files": [], "url": "https://evgeniyvinokurov.pythonanywhere.com/catalog/", "github": True},
     {"name": "veggy-farm", "tags": ["python", "django", "catalog", "markup", "docker", "sqlite"], "files": [], "github": True},
